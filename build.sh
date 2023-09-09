3737be0bf5cd2b29304f151637b31db2606d2ede
Gary J <github@accu.ca>
2023-09-09 09:02:08 -0700

    Add Squash

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ DIR=`dirname $0`
 
 echo
 echo "Building ivr-fastagi-base"
-podman build  -t ivr-fastagi-base -f=$DIR/ivr-fastagi-base/Dockerfile
+podman build --squash -t ivr-fastagi-base -f=$DIR/ivr-fastagi-base/Dockerfile
 
 echo
 echo "Building ivr-asterisk-base"
-podman build  -t ivr-asterisk-base -f=$DIR/ivr-asterisk-base/Dockerfile
+podman build --squash -t ivr-asterisk-base -f=$DIR/ivr-asterisk-base/Dockerfile
