5976df6756d4f5adb299a54bde7742833651060d
Gary J <github@accu.ca>
2023-09-09 09:00:35 -0700

    Change build order

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 DIR=`dirname $0`
 
 echo
-echo "Building ivr-asterisk-base"
-podman build  -t ivr-asterisk-base -f=$DIR/ivr-asterisk-base/Dockerfile
-
-echo
 echo "Building ivr-fastagi-base"
 podman build  -t ivr-fastagi-base -f=$DIR/ivr-fastagi-base/Dockerfile
+
+echo
+echo "Building ivr-asterisk-base"
+podman build  -t ivr-asterisk-base -f=$DIR/ivr-asterisk-base/Dockerfile
